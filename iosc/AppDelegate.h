89bddf6c600b047736656c7b3efb25951107644f
Michael Frister <michael+git@frister.net>
2012-10-27 12:51:28 +0000

    Initial Commit

---
+++ iosc/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  iosc
+//
+//  Created by mf on 27.10.12.
+//  Copyright (c) 2012 Michael Frister. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ iosc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iosc
+//
+//  Created by mf on 27.10.12.
+//  Copyright (c) 2012 Michael Frister. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
